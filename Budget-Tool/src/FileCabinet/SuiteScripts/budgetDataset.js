9073e89728dd98a89d5993ed20efc8858ddfee43
jbucsa <justin.bucsa@gmail.com>
2024-11-16 14:19:59 -0600

    Added a condition that only shows values for the year of 2022.

--- Budget-Tool/src/FileCabinet/SuiteScripts/budgetDataset.js
+++ Budget-Tool/src/FileCabinet/SuiteScripts/budgetDataset.js
@@ -3,16 +3,17 @@
  * @NScriptType DataSetBuilderPlugin
  * @NApiVersion 2.1
  */
-define(["require", "exports", "N/dataset"], function (require, exports, dataset) {
+define(["require", "exports", "N/dataset", "N/query"], function (require, exports, dataset, query) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     exports.createDataset = createDataset;
-    // import query = require("N/query");
     function createDataset(context) {
         const budgetLines = dataset.createJoin({ fieldId: 'budgetmachine' });
         context.dataset = dataset.create({
             type: 'budgets',
             name: 'HITC Budget (SDF)',
+            // Note in this example, '358' is the value for query condition that only shows values for 2022. This value may change from company to company. Check XML export of a filter to see what the query condition is set too. 
+            condition: dataset.createCondition({ column: dataset.createColumn({ fieldId: 'year' }), operator: query.Operator.ANY_OF, values: ['358'] }),
             columns: [
                 dataset.createColumn({ fieldId: 'account', alias: 'Account' }),
                 dataset.createColumn({ fieldId: 'department', alias: 'Department' }),
--- Budget-Tool/src/TypeScripts/budgetDataset.ts
+++ Budget-Tool/src/TypeScripts/budgetDataset.ts
@@ -6,13 +6,16 @@
 
 import {EntryPoints} from "N/types";
 import dataset = require("N/dataset");
-// import query = require("N/query");
+// Query operator is used to create a condition that allows us to use filters.
+import query = require("N/query");
 
 export function createDataset(context: EntryPoints.Plugins.DatasetBuilder.createDatasetContext){
     const budgetLines =  dataset.createJoin({fieldId: 'budgetmachine'});
     context.dataset = dataset.create({
         type: 'budgets',
         name: 'HITC Budget (SDF)',
+        // Note in this example, '358' is the value for query condition that only shows values for 2022. This value may change from company to company. Check XML export of a filter to see what the query condition is set too. 
+        condition: dataset.createCondition({column: dataset.createColumn({fieldId:'year'}), operator:query.Operator.ANY_OF, values:['358']}),
         columns:[
         dataset.createColumn({fieldId:'account', alias:'Account'}),
         dataset.createColumn({fieldId:'department', alias:'Department'}),
